e6728e0326ebd1fcc9e8169de36b2b4ae0cc9d27
Maged Groups <magedyaseengroups@gmail.com>
2026-02-17 21:38:42 +0200

    Update SupplierController

--- App/Http/Controllers/SupplierController.php
+++ App/Http/Controllers/SupplierController.php
@@ -7,4 +7,8 @@ class SupplierController extends Controller
     public static string $name;
     public static string $address;
     public static string $city;
+
+    public static function order (float $amout, int $itemsCount) : void {
+        // 
+    }
 }
