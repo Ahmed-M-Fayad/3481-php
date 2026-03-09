97034f86acf25f64d37baee8f058b073fd009886
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-03-09 12:00:53 +0200

    Fibonacci Series:  Use a loop to print the first N terms of the Fibonacci sequence, where each number is the sum of the two preceding ones.

--- App/ALY/Challenges/Challenge_17/Loops.php
+++ App/ALY/Challenges/Challenge_17/Loops.php
@@ -8,3 +8,17 @@ for ($x = 1; $x <= 100; $x++){
     $sum += $x;
     }
 echo "Sum of numbers from 1 to 100 is: $sum";
+
+
+#*Fibonacci Series:
+$num1 = 0;
+$num2 = 1;
+$terms = 10;
+
+for($i = 2; $i < $terms; $i++){
+    $num3 = $num1 + $num2;
+    $num1 = $num2;
+    $num2 = $num3;
+}
+
+
